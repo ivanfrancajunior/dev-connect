a8e76bec52e22cd374361434e2c901005ddfda86
Ivan junior <contato.ivanfrancajr@gmail.com>
2024-05-19 22:48:31 -0300

    config mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 dotenv.config();
+connectDB();
 const server = express();
 
 server.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+server.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3011;
 
 server.listen(PORT, () => {
-  console.log("server running on port", PORT);
+  console.log('server running on port', PORT);
 });
 
-server.get("/api", (req, res) => {
-  res.json({ message: "hello world!" });
+server.get('/api', (req, res) => {
+  res.json({ message: 'hello world!' });
 });
