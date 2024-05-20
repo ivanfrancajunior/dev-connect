cc0b15b6f16bbba45d41f200b6e10ebc79a06e73
Ivan junior <contato.ivanfrancajr@gmail.com>
2024-05-19 23:08:18 -0300

    config routes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 dotenv.config();
 connectDB();
+
+import authRouter from './routes/api/auth.js';
+import postsRouter from './routes/api/posts.js';
+import profileRouter from './routes/api/profile.js';
+import usersRouter from './routes/api/users.js';
+
 const server = express();
 
 server.use(express.json());
@@ -18,3 +24,8 @@ server.listen(PORT, () => {
 server.get('/api', (req, res) => {
   res.json({ message: 'hello world!' });
 });
+
+server.use('/api/auth', authRouter);
+server.use('/api/posts', postsRouter);
+server.use('/api/profile', profileRouter);
+server.use('/api/users', usersRouter);
